9a66c8382b64c8a75569d6f5e7ee1b1f725a429e
OdionOseiwe <oseiweifebhor@gmail.com>
2022-10-13 14:58:22 +0100

    edited link

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-<<<<<<< HEAD
 // import "./global.css";
 import Main from "./routes/Main";
 import Bond from "./routes/Bond";
@@ -17,13 +16,4 @@ root.render(
         <Route path="bond/:id" element={<Bond />} /> 
       </Routes>
     </BrowserRouter>
-=======
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
->>>>>>> cf37b9e6ef02d5c28b699fd869749f54ae5f660f
 );
